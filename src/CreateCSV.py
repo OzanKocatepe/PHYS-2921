38d7ed19eb29a951d642bc1d9c6078dc01c10a3e
Ozan Kocatepe <okoc0876@wu.physics.usyd.edu.au>
2025-07-06 21:32:53 +1000

    Changed the index.rst to hopefully clean up the docs website and added functionality to actually extract data from a folder in CreateCSV.py. Next step is to update/create new CSV reading and writing functions so that I can work with dicts instead of lists.

--- src/CreateCSV.py
+++ src/CreateCSV.py
@@ -1,13 +1,24 @@
 import sys
+from dataparsing import Manipulation, DataIO
 
 # Checks if this is __main__ so that sphinx doesn't run the code
 # when generating docs.
 if __name__ == "__main__":
 
-    # Looks for the command flags.
-    for arg in sys.argv:
+    # Initialises the flags.
+    inputFolder = None
+    outputFile = None
+    includeFourVectors = False
+    includeDeltaR = False
+    includeInvariantMass = False
 
-        if (arg == "help"):
+    print(sys.argv)
+
+    # Looks for the command flags.
+    for i in range(1, len(sys.argv)):
+
+        # Outputs the help page from the file that it is stored in.
+        if (sys.argv[i] == "help"):
             
             file = open("HelpMessage.txt", 'r')
             helpLines = file.readlines()
@@ -17,3 +28,35 @@ if __name__ == "__main__":
             for line in helpLines:
                 print(line, end="")
             print("\n")
+            exit()
+
+        elif (sys.argv[i] == "-o"):
+            # Gets the next flag as the output file name.
+            outputFile = sys.argv[i + 1]
+
+        elif (sys.argv[i] == "-v"):
+            includeFourVectors = True
+
+        elif (sys.argv[i] == "-r"):
+            includeDeltaR = True
+
+        elif (sys.argv[i] == "-m"):
+            includeInvariantMass = True
+
+        # If its not a flag, and its not the output file name, then its the input folder.
+        else:
+            if (sys.argv[i - 1] != "-o"):
+                inputFolder = sys.argv[i]
+
+    # Handles errors.
+    if (inputFolder == None):
+        raise Exception("No input file specified.")
+    if (outputFile == None):
+        raise Exception("No output file specified.")
+    
+    # Extracts the collision data from the input folder.
+    collisions = DataIO.ExtractCollisionsFromFolder(inputFolder)
+    collisions = [collision.Filter([6, -6]) for collision in collisions]
+
+    # Gets the CSV data and stores it in a list of dicts.
+    csvData = Manipulation.ExtractAttributes(collisions, includeFourVectors, includeDeltaR, includeInvariantMass)
