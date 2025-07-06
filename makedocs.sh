b7e74eab239ca98f43b8f4a257bee1b7f2e06bb6
Ozan Kocatepe <okoc0876@wu.physics.usyd.edu.au>
2025-07-06 20:34:55 +1000

    Updated the docs and also the makedocs bash file to truly reset all the non-important sphinx files.

--- makedocs.sh
+++ makedocs.sh
@@ -1,3 +1,18 @@
+# Moves important files out of sphinx.
+mv sphinx/conf.py ..
+mv sphinx/index.rst ..
+mv sphinx/make.bat ..
+mv sphinx/Makefile ..
+
+# Delete all previous sphinx files.
+rm -r sphinx/*
+
+# Moves important files back.
+mv conf.py sphinx/
+mv index.rst sphinx/
+mv make.bat sphinx/
+mv Makefile sphinx/
+
 # Make documentation.
 sphinx-apidoc -o sphinx src/
 cd sphinx
--- sphinx/modules.rst
+++ sphinx/modules.rst
@@ -4,3 +4,8 @@ src
 .. toctree::
    :maxdepth: 4
 
+   dataparsing
+   mainData
+   mainNetwork
+   mainPlotting
+   objects
